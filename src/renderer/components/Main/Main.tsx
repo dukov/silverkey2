04e8ceb3c24e9f5e7f1e7ce06c801e040df7b113
Dmitry Ukov <dukov@mirantis.com>
2023-01-21 22:48:55 +0600

    Simplify main component
    Change-Id: If15679b538b6920362e9db8be9160a04ea9073ef

--- src/renderer/components/Main/Main.tsx
+++ src/renderer/components/Main/Main.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 import "./Main.css";
 
-import Delimiter from "../Delimiter/Delimiter";
 import SearchRow from "../SearchRow/SearchRow";
-import Footer from "../FooterRow/Footer";
 import ResultRows from "../ResultRows/ResultRows";
 
 type MainState = {
@@ -37,9 +35,9 @@ class Main extends React.Component<{}, MainState> {
     return (
       <div className="main">
         <SearchRow filterKeys={this.filterKeys} />
-        <Delimiter />
+        <div className="delimiter"></div>
         <ResultRows resultKeys={this.state.filteredKeys} />
-        <Footer />
+        <div className="footer"></div>
       </div>
     );
   }
