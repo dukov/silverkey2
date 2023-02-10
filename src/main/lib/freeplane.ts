e3b8f4c27df2f979b1c868b58352d7b99b7d262f
Dmitry Ukov <dukov@mirantis.com>
2023-02-11 00:43:13 +0600

    Freeplan runner
    Change-Id: I063b1cbe4df672994930d1499a2c102c38c81e18

---
+++ src/main/lib/freeplane.ts
@@ -0,0 +1,52 @@
+import fs from "fs";
+import { execFile, ChildProcess } from "child_process";
+
+const MAC_FREEPLANE_DEFAULT_PATH =
+  "/Applications/Freeplane.app/Contents/MacOS/Freeplane";
+
+class FreePlaneRunner {
+  path: string | null;
+  freplane: ChildProcess | null = null;
+  constructor() {
+    this.path = this.findFreePlaneBinary();
+  }
+  private findFreePlaneBinary(): string | null {
+    if (process.platform == "darwin") {
+      return this.findFreePlaneMac();
+    } else {
+      return null;
+    }
+  }
+  private findFreePlaneMac(): string | null {
+    try {
+      fs.statSync(MAC_FREEPLANE_DEFAULT_PATH);
+    } catch (e) {
+      console.log(
+        `Freeplane not found in default path ${MAC_FREEPLANE_DEFAULT_PATH}`
+      );
+      return null;
+    }
+    return MAC_FREEPLANE_DEFAULT_PATH;
+  }
+  private spawnFreeplaneProcess(path: string) {
+    const fp = execFile(path);
+    fp.on("spawn", () => {
+      console.log("Freeplane Process started");
+      this.freplane = fp;
+    });
+    fp.on("exit", () => {
+      console.log("Freeplane Process stopped");
+      this.freplane = null;
+    });
+  }
+  run() {
+    if (this.path) {
+      this.spawnFreeplaneProcess(this.path);
+    }
+  }
+  stop() {
+    if (this.freplane) {
+      this.freplane.kill();
+    }
+  }
+}
